7058a6c9ab9cb0598d808fca34c9b8b9814b5345
DevEmmy <eolaosebikan60@gmail.com>
2022-03-24 11:49:22 -0500

    Extraction of data from API incomplete

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,28 +1,41 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import TransactionDetails from './TransactionDetails'
 
 function Form() {
     const [ checkTransaction, setCheckTransaction ] = useState(false)
+    const [walletDetails, setWalletDetails ] = useState([])
+
+    const walletRef = useRef(null);
+    const blockRef = useRef(null);
+    const apiKey = process.env.API_KEY
+
+    
 
 
     const setCheckTransactionStatus = ()=>{
-        setCheckTransaction(true)
+        setCheckTransaction(!checkTransaction)
         console.log('No')
     }
 
-    // const fetchDetails = (e)=>{
-    //     e.preventDefault()
-    // }
+    const fetchDetails = async ()=>{
+        const url = `https://api.etherscan.io/api?module=account&action=balance&address=${walletRef.current.value}&tag=latest&apikey=${apiKey}`
+
+        const resp = await fetch(url);
+        // const data = resp.json()
+        // setWalletDetails(data.response);
+        console.log(resp.json())
+    }
+
   return (
     <div className='form_compo'>
         <h3>Check Details</h3>
         <form onSubmit={(e)=>e.preventDefault()}>
-            <input type="text" placeholder='Wallet Address'/>
-            <input type="text" placeholder='Block' />
-            <button onClick={()=> {setCheckTransactionStatus();}}> Check </button>
+            <input type="text" placeholder='Wallet Address' ref={walletRef}/>
+            <input type="text" placeholder='Block' ref={blockRef}/>
+            <button onClick={()=> {setCheckTransactionStatus(); fetchDetails();}}> Check </button>
         </form>
 
-        { checkTransaction ?  <TransactionDetails /> : ""}
+        { checkTransaction ?  <TransactionDetails closeFunc = {setCheckTransactionStatus}/> : ""}
     </div>
   )
 }
--- src/Components/TransactionDetails.jsx
+++ src/Components/TransactionDetails.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 
-function TransactionDetails() {
+function TransactionDetails({closeFunc}) {
   return (
     <div className='transaction_compo'>
         <div className="dark_bg" />
+        <div className="cancel_dialogue" onClick={()=>{ closeFunc() }}>
+          x
+        </div>
         <div className="transaction_container">
-          
+
            <div className="transaction_content">
-           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt, ab numquam? Maxime laborum dolore quisquam fugit quidem deserunt voluptates. Eveniet.
+            <div className="wallet_details">
+              <h3>Wallet Details</h3>
+            </div>
+
+            <div className="transactions_details">
+              <h3>Transactions Details</h3>
+            </div>
            </div>
         </div>
     </div>
