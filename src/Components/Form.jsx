fb74919d634d71d5287d23ecc5f8523ba91db67c
DevEmmy <eolaosebikan60@gmail.com>
2022-03-24 22:15:08 -0500

    fixed fetch api issue

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,9 +1,10 @@
-import React, { useState, useRef } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import TransactionDetails from './TransactionDetails'
 
 function Form() {
     const [ checkTransaction, setCheckTransaction ] = useState(false)
     const [walletDetails, setWalletDetails ] = useState([])
+    const [ walletTransactions, setWalletTransactions ] = useState([])
 
     const walletRef = useRef(null);
     const blockRef = useRef(null);
@@ -21,21 +22,33 @@ function Form() {
         const url = `https://api.etherscan.io/api?module=account&action=balance&address=${walletRef.current.value}&tag=latest&apikey=${apiKey}`
 
         const resp = await fetch(url);
-        // const data = resp.json()
-        // setWalletDetails(data.response);
-        console.log(resp.json())
+        const data = await resp.json()
+        setWalletDetails(data.response);
+        console.log(data)
     }
 
+        
+    const fetchTransactionDetails = async ()=>{
+        const url = `https://api.etherscan.io/api?module=account&action=txlist&address=${walletRef.current.value}&startblock=${blockRef.current.value}&endblock=99999999&page=1&offset=10&sort=asc&apikey=${apiKey}`
+
+        const resp = await fetch(url);
+        const data = await resp.json()
+        setWalletTransactions(data.result);
+        console.log(data.result)
+    }
+
+    // useEffect(fetchTransactionDetails(), [walletTransactions])
+    
   return (
     <div className='form_compo'>
         <h3>Check Details</h3>
         <form onSubmit={(e)=>e.preventDefault()}>
             <input type="text" placeholder='Wallet Address' ref={walletRef}/>
             <input type="text" placeholder='Block' ref={blockRef}/>
-            <button onClick={()=> {setCheckTransactionStatus(); fetchDetails();}}> Check </button>
+            <button onClick={()=> {setCheckTransactionStatus(); fetchTransactionDetails()}}> Check </button>
         </form>
 
-        { checkTransaction ?  <TransactionDetails closeFunc = {setCheckTransactionStatus}/> : ""}
+        { checkTransaction ?  <TransactionDetails closeFunc = {setCheckTransactionStatus} transactions={walletTransactions} walletAddress = { walletRef.current.value }/> : ""}
     </div>
   )
 }
--- src/Components/TransactionDetails.jsx
+++ src/Components/TransactionDetails.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function TransactionDetails({closeFunc}) {
+function TransactionDetails({closeFunc, transactions, walletAddress}) {
   return (
     <div className='transaction_compo'>
         <div className="dark_bg" />
@@ -10,12 +10,28 @@ function TransactionDetails({closeFunc}) {
         <div className="transaction_container">
 
            <div className="transaction_content">
-            <div className="wallet_details">
+            {/* <div className="wallet_details">
               <h3>Wallet Details</h3>
-            </div>
+            </div> */}
 
+            { console.log(transactions)}
             <div className="transactions_details">
               <h3>Transactions Details</h3>
+              <div className="details">
+                
+                {
+                  transactions?.map((transaction)=>{
+                    return(
+                      <div className="details">
+                        <p className="status">{ transaction.from == walletAddress ? "Sent" : "Received"}</p>
+                        <p className="other_wallet">
+                        { transaction.from == walletAddress ? `to : ${transaction.to}` : `from : ${transaction.from}`}
+                        </p>
+                      </div>
+                    )
+                  })
+                }
+              </div>
             </div>
            </div>
         </div>
